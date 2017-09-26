dfc3729a4cfce1146bdaadd92fbaaaace47563f4
Dmitry Mozzherin <dmozzherin@gmail.com>
2017-09-26 09:33:08 -0500

    ready for gnrd

--- finder/finder.go
+++ finder/finder.go
@@ -11,19 +11,51 @@ import (
 
 func ProcessTitles(db *sql.DB) {
 	titleIDs := make(chan int)
+	findQueue := make(chan *models.Title)
 	counter := make(chan int)
-	var wg sync.WaitGroup
+	results := make(chan *models.Title)
+	var wgLoad sync.WaitGroup
+	var wgFind sync.WaitGroup
+	var wgFinish sync.WaitGroup
 
 	go counterLog(counter)
 
-	for i := 1; i <= 20; i++ {
-		wg.Add(1)
-		go titleWorker(db, &wg, titleIDs, counter)
+	for i := 1; i <= 10; i++ {
+		wgLoad.Add(1)
+		go titleWorker(db, &wgLoad, titleIDs, findQueue, counter)
 	}
 
+	for i := 1; i <= 50; i++ {
+		wgFind.Add(1)
+		go finderWorker(&wgFind, findQueue, results)
+	}
+
+	wgFinish.Add(1)
+	go saveFoundNames(db, &wgFinish, results)
+
 	loader.ImportTitles(db, titleIDs)
-	wg.Wait()
+
+	wgLoad.Wait()
 	close(counter)
+	close(findQueue)
+	wgFind.Wait()
+	close(results)
+	wgFinish.Wait()
+}
+
+func saveFoundNames(db *sql.DB, wg *sync.WaitGroup,
+	results <-chan *models.Title) {
+	defer wg.Done()
+	for _ = range results {
+	}
+}
+
+func finderWorker(wg *sync.WaitGroup, findQueue <-chan *models.Title,
+	results chan<- *models.Title) {
+	defer wg.Done()
+	for t := range findQueue {
+		results <- t
+	}
 }
 
 func counterLog(counter <-chan int) {
@@ -37,11 +69,12 @@ func counterLog(counter <-chan int) {
 }
 
 func titleWorker(db *sql.DB, wg *sync.WaitGroup, titleIDs <-chan int,
-	counter chan<- int) {
+	resQueue chan<- *models.Title, counter chan<- int) {
 	defer wg.Done()
 	for titleID := range titleIDs {
 		title := models.TitleFind(db, titleID)
 		loader.ImportPages(db, &title)
+		resQueue <- &title
 		counter <- 1
 	}
 }
